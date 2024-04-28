1e6f51d072f5c66bb1fa8e1c21d15c1299897844
10yii <10yii@gmail.com>
2024-04-28 23:04:12 +0900

    Disable Image Optimization

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     output: "export",
     distDir: "out",
-
+    images: { unoptimized: true }
 
 }
 
